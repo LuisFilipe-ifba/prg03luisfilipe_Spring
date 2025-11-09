dbd59e1c39e67b4a15ea7de4a152ccf40f031836
PC <PC@DESKTOP-3NNTM0F>
2025-11-08 23:15:34 -0300

    adicionado persistence em curso(gastei mais tempo que o esperado nisso)

--- src/main/java/br/com/ifba/curso/CursoSave.java
+++ src/main/java/br/com/ifba/curso/CursoSave.java
@@ -2,19 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package br.com.ifba.atividade09.interfaces;
+package br.com.ifba.curso;
 
 /**
  *
  * @author PC
  */
-public interface Pagamento {
-
-    /**
-     *
-     * @param valor
-     * @return
-     */
-    public double calcularTotal(double valor);
-    public String imprimirRecibo(double valor); 
+public class CursoSave {
+    
 }
--- src/main/java/br/com/ifba/curso/entity/Curso.java
+++ src/main/java/br/com/ifba/curso/entity/Curso.java
@@ -2,16 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package br.com.ifba.atividade10.classes;
+package br.com.ifba.curso.entity;
 
 /**
  *
  * @author PC
  */
-public class Forma2D extends Forma{
-    
-    public double obterArea(double altura, double largura){
-        return altura + largura;
-    }
+public class Curso {
     
 }
