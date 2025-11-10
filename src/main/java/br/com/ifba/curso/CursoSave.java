1031081b2f1e023f114f3b05c24d326c6bea25b5
PC <PC@DESKTOP-3NNTM0F>
2025-11-09 21:17:33 -0300

    adicionado "Curso" não consegui terminar a atividade 11 ,não soube mexer com scroll panel e fazer o JFrame exibir informações do curso

--- src/main/java/br/com/ifba/curso/CursoSave.java
+++ src/main/java/br/com/ifba/curso/CursoSave.java
@@ -4,10 +4,50 @@
  */
 package br.com.ifba.curso;
 
+import br.com.ifba.curso.entity.Curso;
+import javax.persistence.EntityManager; // Import needed
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 /**
  *
  * @author PC
  */
 public class CursoSave {
-    
+
+    //abaixo Cria o EntityManeger
+    private static final EntityManagerFactory entityManagerFactory
+            = Persistence.createEntityManagerFactory("gerenciamento_curso");
+
+    public void cursoSalvar(Curso curso) {
+        //declara o entityManeger
+        EntityManager entityManager = null;
+
+        try {
+            
+            entityManager = entityManagerFactory.createEntityManager();
+
+            entityManager.getTransaction().begin();
+
+            entityManager.persist(curso);
+            
+            entityManager.getTransaction().commit();
+            
+            System.out.println("Curso salvo com sucesso!");
+
+        } catch (Exception e) {
+            //Exeção caso ocora um erro
+            if (entityManager != null && entityManager.getTransaction().isActive()) {
+                entityManager.getTransaction().rollback();
+            }
+            e.printStackTrace();
+            System.err.println("Erro ao salvar o curso: " + e.getMessage());
+
+        } finally {
+            //Termina fechando o curso
+            if (entityManager != null) {
+                entityManager.close();
+            }
+        }
+    }
 }
--- src/main/java/br/com/ifba/curso/entity/Curso.java
+++ src/main/java/br/com/ifba/curso/entity/Curso.java
@@ -4,10 +4,70 @@
  */
 package br.com.ifba.curso.entity;
 
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GenerationType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 /**
  *
  * @author PC
  */
-public class Curso {
+@Entity
+public class Curso{
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    
+    private String nome;
+    private String codigoCurso;
+    private boolean ativo;
+    
+    //metodos
+
+    public Curso(long id, String nome, String codigoCurso) {
+        this.id = id;
+        this.nome = nome;
+        this.codigoCurso = codigoCurso;
+        this.ativo = true;
+    }
+
+    
+    
+    public long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCodigoCurso() {
+        return codigoCurso;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCodigoCurso(String codigoCurso) {
+        this.codigoCurso = codigoCurso;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+    
+    
     
 }
